f89665c4d4857cbabb16ad802b8cadbdd584d4f0
ream <1069700995@qq.com>
2013-12-15 02:06:23 +0800

    build class

--- con_db.py
+++ con_db.py
@@ -4,25 +4,50 @@ __author__ = 'ST'
 from sqlalchemy import create_engine
 import zlib
 
-engine2 = create_engine('sqlite:///2.db')
 
-q = engine2.execute('select _id, fromAddress, subject, bodyCompressed from messages')
+class con_db():
+    def __init__(self, path):
+        self.engine = create_engine('sqlite:///' + path)
 
-ret = []
-for row in q:
-    if row[3]:
-        body = '<html><body>' + zlib.decompress(row[3]) + '</body></html>'
-    else:
-        body = ''
+    def get_mail_list(self, begin=None, end=None):
+        limit = ''
+        if begin and end:
+            limit = 'limit %d, %d' % (begin, end)
 
-    ret.append({'id': row[0], 'from_address': row[1], 'subject': row[2],
-                'body': body})
+        sql = 'select _id, fromAddress, subject, bodyCompressed, ' \
+              'toAddresses, joinedAttachmentInfos  from messages ' + limit
 
-for i in ret:
-    print i.get('id')
-    print i.get('from_address')
-    print i.get('subject')
-    print i.get('body')
-    f = open(str(i.get('id')) + '.html', 'w')
-    f.write(i.get('body'))
-    f.close()
+        query = self.engine.execute(sql)
+
+        ret = []
+        for row in query:
+            attachments = self.get_attachments(row[5])
+            if row[3]:
+                body = '<html><body>' + zlib.decompress(row[3]) + '</body></html>'
+            else:
+                body = ''
+            ret.append({'id': row[0], 'from_address': row[1], 'subject': row[2],
+                        'body': body, 'to_address': row[4], 'attachments': attachments})
+
+        return ret
+
+    def get_attachments(self, info):
+        ret = []
+        attach_info = ''
+        if info:
+            attach_info = info.split('|')[6::7]
+        for item in attach_info:
+            sql = "select distinct filename from attachments where originExtras = '" + item + "'"
+            query = self.engine.execute(sql)
+            for row in query:
+                ret.append({'filename': row[0]})
+        return ret
+
+
+if __name__ == '__main__':
+    path = r'C:\Users\ST\Documents\GitHub\MailManager' \
+           r'\com.google.android.gm\databases\mailstore.funssuse@gmail.com.db'
+    ins = con_db(path)
+    query = ins.get_mail_list(10, 20)
+    for i in query:
+        print i.get('attachments')
