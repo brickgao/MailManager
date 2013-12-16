9dcf14020bef19d26b0c80b8df6dbcadcf135cc2
Brickgao <brickgao@gmail.com>
2013-12-16 14:05:13 +0800

    finish getMailList

--- main.py
+++ main.py
@@ -16,17 +16,37 @@ class QMainArea(QtGui.QWidget):
         grid = QtGui.QGridLayout()
         grid.setSpacing(5)
 
-
-        self.AddressList = QtGui.QListWidget()
-
         self.MailList = QtGui.QTreeWidget()
         self.MailList.setHeaderLabels([u'时间', u'收件人',
                                        u'发件人', u'标题', u'预览'])
+        self.MailList.setColumnWidth(0, 120)
+        self.MailList.setColumnWidth(1, 200)
+        self.MailList.setColumnWidth(2, 200)
+        self.MailList.setColumnWidth(3, 100)
 
         grid.addWidget(self.MailList)
 
-
         self.setLayout(grid) 
+
+    def updateInfo(self, list_db):
+
+        self.MailList.clear() 
+        for i in range(len(list_db)):
+
+            MailInfo = QtGui.QTreeWidgetItem()
+            receive_date = list_db[i]['receive_date']
+            date_s = str(receive_date.year) + '-' + \
+                     str(receive_date.month) + '-' + \
+                     str(receive_date.day) + ' ' + \
+                     str(receive_date.hour) + ':' + \
+                     str(receive_date.second)
+            MailInfo.setText(0, date_s)
+            MailInfo.setText(1, list_db[i]['to_address'])
+            MailInfo.setText(2, list_db[i]['from_address'])
+            MailInfo.setText(3, list_db[i]['subject'])
+            MailInfo.setText(4, list_db[i]['snippet'])
+            self.MailList.addTopLevelItem(MailInfo)
+
 
 class MainWindow(QtGui.QMainWindow):
 
@@ -69,7 +89,29 @@ class MainWindow(QtGui.QMainWindow):
     def inputfile(self):
 
         fname = QtGui.QFileDialog.getOpenFileName(self, u'打开文件')
-        self.db = con_db(fname)
+        sfname = str(fname)
+        if sfname == '':
+            return
+        try:
+            self.db = con_db(sfname)
+        except Exception, e:
+            print e
+            self.ErrorAlert(u'打开 db 错误，请选择正确的 db 文件')
+        else:
+            try:
+                self.list_db = self.db.get_mail_list()
+            except Exception, e:
+                print e
+                self.ErrorAlert(u'解析 db 错误，请选择正确的 db 文件')
+            else:
+                self.MainArea.updateInfo(self.list_db)
+            
+
+    def ErrorAlert(self, s):  
+        QtGui.QMessageBox.critical(self, u'错误', s)  
+        
+    
+            
 
 def main():
     
