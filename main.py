8fd044511e51559a4da98bf4e5dc52195ba49703
Brickgao <brickgao@gmail.com>
2013-12-16 19:04:37 +0800

    finish FileSave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import sys, os, eml_gen
+import sys, os, eml_gen, shutil
 from con_db import con_db
 from PyQt4 import QtGui, QtCore, QtWebKit
 
@@ -14,36 +14,37 @@ class QMailView(QtGui.QDialog):
 
     def initLayout(self):
         
-        from_address = QtGui.QLabel(u'发信人：')
-        from_address_val = QtGui.QLabel(self.MailMsg['from_address'])
+        self.from_address = QtGui.QLabel(u'发信人：')
+        self.from_address_val = QtGui.QLabel(self.MailMsg['from_address'])
 
-        to_address = QtGui.QLabel(u'收信人：')
-        to_address_val = QtGui.QLabel(self.MailMsg['to_address'])
+        self.to_address = QtGui.QLabel(u'收信人：')
+        self.to_address_val = QtGui.QLabel(self.MailMsg['to_address'])
 
-        subject = QtGui.QLabel(u'标题：')
-        subject_val = QtGui.QLabel(self.MailMsg['subject'])
+        self.subject = QtGui.QLabel(u'标题：')
+        self.subject_val = QtGui.QLabel(self.MailMsg['subject'])
 
-        receive_date = QtGui.QLabel(u'收信时间：')
-        receive_date_val = self.MailMsg['receive_date']
-        date_s = QtGui.QLabel(
-                    str(receive_date_val.year) + '-' + \
-                    str(receive_date_val.month) + '-' + \
-                    str(receive_date_val.day) + ' ' + \
-                    str(receive_date_val.hour) + ':' + \
-                    str(receive_date_val.second))
+        self.receive_date = QtGui.QLabel(u'收信时间：')
+        self.receive_date_val = self.MailMsg['receive_date']
+        self.date_s = QtGui.QLabel(
+                    str(self.receive_date_val.year) + '-' + \
+                    str(self.receive_date_val.month) + '-' + \
+                    str(self.receive_date_val.day) + ' ' + \
+                    str(self.receive_date_val.hour) + ':' + \
+                    str(self.receive_date_val.second))
 
-        ContentView = QtGui.QLabel(u'邮件正文：')
-        ContentView_val = QtWebKit.QWebView()
+        self.ContentView = QtGui.QLabel(u'邮件正文：')
+        self.ContentView_val = QtWebKit.QWebView()
         HtmlCode = unicode(self.MailMsg['body'], 'utf-8')
-        ContentView_val.setHtml(HtmlCode, 
+        self.ContentView_val.setHtml(HtmlCode, 
                             QtCore.QUrl(''))
 
-        attachmentsList = QtGui.QLabel(u'附件：')
-        attachmentsList_val = QtGui.QTreeWidget()
-        attachmentsList_val.setHeaderLabels([u'#' ,u'文件名', 
+        self.attachmentsList = QtGui.QLabel(u'附件：')
+        self.attachmentsList_val = QtGui.QTreeWidget()
+        self.attachmentsList_val.setHeaderLabels([u'#' ,u'文件名', 
                                              u'文件大小', u'是否存在',
                                              u'文件路径'])
-        attachmentsList_val.setColumnWidth(0, 60)
+        self.attachmentsList_val.setColumnWidth(0, 60)
+        self.attachmentsList_val.itemDoubleClicked.connect(self.attachmentsListouput)
         for i in range(len(self.MailMsg['attachments'])):
             FileInfo = QtGui.QTreeWidgetItem()
             FileInfo.setText(0, str(i))
@@ -55,27 +56,27 @@ class QMailView(QtGui.QDialog):
                 FileInfo.setText(4, filepath)
             else:
                 FileInfo.setText(4, u'文件不存在')
-            attachmentsList_val.addTopLevelItem(FileInfo)
+            self.attachmentsList_val.addTopLevelItem(FileInfo)
 
-        MailoutputBtn = QtGui.QPushButton(u'导出当前文件')
-        MailoutputBtn.clicked.connect(self.Mailoutput)
+        self.MailoutputBtn = QtGui.QPushButton(u'导出当前文件')
+        self.MailoutputBtn.clicked.connect(self.Mailoutput)
 
         grid = QtGui.QGridLayout()
         grid.setSpacing(10)
 
-        grid.addWidget(from_address, 1, 0)
-        grid.addWidget(from_address_val, 1, 1)
-        grid.addWidget(to_address, 2, 0)
-        grid.addWidget(to_address_val, 2, 1)
-        grid.addWidget(subject, 3, 0)
-        grid.addWidget(subject_val, 3, 1)
-        grid.addWidget(receive_date, 4, 0)
-        grid.addWidget(date_s, 4, 1)
-        grid.addWidget(attachmentsList, 5, 0)
-        grid.addWidget(attachmentsList_val, 6, 0, 1, 3)
-        grid.addWidget(ContentView, 7, 0)
-        grid.addWidget(MailoutputBtn, 7, 2)
-        grid.addWidget(ContentView_val, 8, 0, 1, 3)
+        grid.addWidget(self.from_address, 1, 0)
+        grid.addWidget(self.from_address_val, 1, 1)
+        grid.addWidget(self.to_address, 2, 0)
+        grid.addWidget(self.to_address_val, 2, 1)
+        grid.addWidget(self.subject, 3, 0)
+        grid.addWidget(self.subject_val, 3, 1)
+        grid.addWidget(self.receive_date, 4, 0)
+        grid.addWidget(self.date_s, 4, 1)
+        grid.addWidget(self.attachmentsList, 5, 0)
+        grid.addWidget(self.attachmentsList_val, 6, 0, 1, 3)
+        grid.addWidget(self.ContentView, 7, 0)
+        grid.addWidget(self.MailoutputBtn, 7, 2)
+        grid.addWidget(self.ContentView_val, 8, 0, 1, 3)
 
         self.setLayout(grid)
 
@@ -92,6 +93,20 @@ class QMailView(QtGui.QDialog):
         sfname = os.path.abspath(sfname)
         eml_gen.save_to_eml(self.MailMsg, sfname)
 
+    def attachmentsListouput(self):
+
+        Id = int(self.attachmentsList_val.currentItem().text(0))
+        if self.MailMsg['attachments'][Id]['exist'] != True:
+            return self.ErrorAlert(u'请选择存在的文件')
+        fname = QtGui.QFileDialog.getSaveFileName(self, u'保存附件', self.MailMsg['attachments'][Id]['name'], u'*.*')
+        fname = unicode(fname)
+        if fname == '':
+            return
+        shutil.copyfile(self.MailMsg['attachments'][Id]['path'], fname)
+
+    def ErrorAlert(self, s):
+
+        QtGui.QMessageBox.critical(self, u'错误', s)
 
 class QMainArea(QtGui.QWidget):
 
@@ -165,16 +180,12 @@ class MainWindow(QtGui.QMainWindow):
         exitAction.setStatusTip(u'退出 Mail Manager')
         exitAction.triggered.connect(QtGui.qApp.quit)
 
-        outputAction = QtGui.QAction(u'批量导出', self)
-        outputAction.setStatusTip(u'批量导出文件')
-
         inputAction = QtGui.QAction(u'打开', self)
         inputAction.setStatusTip(u'打开文件')
         inputAction.triggered.connect(self.inputfile)
     
         fileMenu = menubar.addMenu(u'文件')
         fileMenu.addAction(inputAction)
-        fileMenu.addAction(outputAction)
         fileMenu.addAction(exitAction)
 
         self.MainArea = QMainArea()
@@ -208,6 +219,7 @@ class MainWindow(QtGui.QMainWindow):
             
 
     def ErrorAlert(self, s):  
+
         QtGui.QMessageBox.critical(self, u'错误', s)  
         
     
